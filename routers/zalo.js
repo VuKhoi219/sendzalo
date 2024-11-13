924e9ad6b5f1052a37cf97923dd6a25d093ccf1d
vukhoi2k5 <vukhoi353@gmail.com>
2024-11-13 09:21:35 +0700

    cap nhat ham moi voi chuc nang gui tin nhăn ngươi theo doi

--- routers/zalo.js
+++ routers/zalo.js
@@ -1,18 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const zaloService = require('../services/zalo');
+const zaloService = require("../services/zalo");
 
-router.post('/send', async (req, res) => {
-    try{
-        const result =  await zaloService.send();
-        // console.log(result);
-        if(result){
-            res.json({messa : "ok"});
-        }
-        res.json({messa : "Lỗi "});
-    } catch (error) {
-        console.log(error);
+router.post("/send", async (req, res) => {
+  try {
+    const result = await zaloService.send(req.body);
+    if (result) {
+      return res.json({ messa: result.data.message });
     }
+    res.json({ messa: "Lỗi " });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.post("/findUser_id", async (req, res) => {
+  try {
+    const result = await zaloService.findUser_id();
+    if (result) {
+      return res.json({ messa: result.data.message });
+    }
+    res.json({ messa: "Listring " });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.post("/sendUserFollow", async (req, res) => {
+  try {
+    const result = await zaloService.sendUserFollow();
+    console.log(result);
+    if (result) {
+      return res.json({ messa: result.data.message });
+    }
+    res.json({ messa: "Lỗi " });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
--- services/zalo.js
+++ services/zalo.js
@@ -1,27 +1,25 @@
 const axios = require("axios");
 require("dotenv").config();
-const jwt = require("jsonwebtoken");
 const accessToken = process.env.ACCESS_TOKEN;
 const baseUrl = process.env.BASE_URL;
+const baseUrl2 = process.env.BASE_URL2;
+const baseUrl3 = process.env.BASE_URL3;
 
-async function send() {
+async function send(body) {
   try {
     const res = await axios.post(
       baseUrl,
       {
         data: {
-          phone: "0901315275",
-          template_id: "7895417a7d3f9461cd2e",
-          template_data: {
-            ky: "1",
-            thang: "4/2020",
-            start_date: "20/03/2020",
-            end_date: "20/04/2020",
-            customer: "Nguyễn Thị Hoàng Anh",
-            cid: "PE010299485",
-            address: "VNG Campus, TP.HCM",
-            amount: "100",
-            total: "100000",
+          phone: "0934459099",
+          template_id: "383148",
+          template_data: {  
+            "order_code": "order_code",
+            "date": "01/08/2020",
+            "price": 100,
+            "name": "name",
+            "phone_number": "phone_number",
+            "status": "status",
           },
           tracking_id: "tracking_id",
         },
@@ -29,16 +27,69 @@ async function send() {
       {
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${accessToken}`,
+          "Access_token": `${accessToken}`,
         },
       }
     );
-    console.log(res);
+    // console.log(res.data);
     return res;
   } catch (error) {
     console.log(error);
   }
 }
+async function findUser_id() {
+  try {
+    const result = await axios.post(
+      baseUrl3,
+      {
+        data:{
+          "offset" : 0,
+          "count" : 10,
+          "tag_name" : "",
+        }
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Access_token": `${accessToken}`,
+        },
+      }
+    );
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function sendUserFollow() {
+  try {
+    const result = await findUser_id();
+    const res = await axios.post(
+      baseUrl2,
+      {
+        recipient : {
+          user_id: result["data"].data[0].user_id,
+        },
+        message : {
+          text: "Hello World"
+        }
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Access_token": `${accessToken}`,
+        },
+      }
+    );
+    return res;
+  } catch (error) {
+    if (error.response) {
+      console.log("Error response:", error.response.data);
+    } else {
+      console.log(error.message);
+    }  }
+}
 module.exports = {
   send,
+  sendUserFollow,
+  findUser_id
 };
